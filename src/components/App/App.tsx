c3bd48564b31f2741c70e4ad7c14ba88f6861c1a
Boris Cherny <boris@performancejs.com>
2022-03-06 15:05:59 -0800

    edit timeline, make draggable

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,23 @@
 import React, {useCallback, useMemo, useState} from 'react'
-import {BLUE_FLIGHTS} from '../../flights'
+import {BLUE_FLIGHTS, RED_FLIGHTS} from '../../flights'
 import computeFeatures, {Feature} from '../../utils/computeFeatures'
 import Legend from '../Legend/Legend'
 import Map from '../Map/Map'
-import {Timeline} from '../Timeline/Timeline'
+import Timeline from '../Timeline/Timeline'
 import './App.css'
 
 function App(): React.ReactElement {
-  const features = useMemo(() => computeFeatures(BLUE_FLIGHTS), [])
-  const [currentFeature, setCurrentFeature] = useState<Feature>(features[0])
+  const blueFeatures = useMemo(() => computeFeatures(BLUE_FLIGHTS), [])
+  const redFeatures = useMemo(() => computeFeatures(RED_FLIGHTS), [])
+  const [currentFeature, setCurrentFeature] = useState<Feature>(blueFeatures[0])
 
   const onFlightDone = useCallback(() => {
-    const i = features.indexOf(currentFeature)
-    const nextFeature = features[i + 1]
+    const i = blueFeatures.indexOf(currentFeature)
+    const nextFeature = blueFeatures[i + 1]
     if (nextFeature) {
       setCurrentFeature(nextFeature)
     }
-  }, [currentFeature, features])
+  }, [currentFeature, blueFeatures])
 
   return (
     <div className="App">
@@ -24,7 +25,7 @@ function App(): React.ReactElement {
       <Legend flight={currentFeature.flight} />
       <Timeline
         currentFeature={currentFeature}
-        features={features}
+        features={[blueFeatures, redFeatures]}
         color="Blue"
       />
     </div>
--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,33 +1,56 @@
 import {ReactElement} from 'react'
 import {Feature} from '../../utils/computeFeatures'
 import './Timeline.css'
+import {DndProvider, useDrag} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
 
 type Props = {
   color: string
   currentFeature: Feature
-  features: Feature[]
+  features: Feature[][]
 }
 
 // Assumes features are in order of date
 // Assumes features has length >= 1
-export function Timeline({
-  color,
-  currentFeature,
-  features,
-}: Props): ReactElement {
-  const minDate = features[0].flight.date.getTime()
-  const maxDate = features[features.length - 1].flight.date.getTime()
+function Timeline({color, currentFeature, features}: Props): ReactElement {
+  const minDate = features.reduce(
+    (p, [_]) => Math.min(p, _.flight.date.getTime()),
+    Infinity
+  )
+  const maxDate = features.reduce(
+    (p, _) => Math.max(p, _[_.length - 1].flight.date.getTime()),
+    -Infinity
+  )
+
+  const [{opacity}, dragRef] = useDrag(
+    () => ({
+      type: 'CARD',
+      collect(monitor) {
+        return {
+          opacity: monitor.isDragging() ? 0.5 : 1,
+        }
+      },
+    }),
+    []
+  )
+
   return (
     <div className="Timeline">
-      {features.map((_) => (
-        <div
-          className={`Flight ${color}`}
-          key={_.id}
-          style={{left: getLeft(_.flight.date)}}
-        />
-      ))}
+      {features.flatMap((f, i) =>
+        f.map((_) => (
+          <div
+            className={`Flight ${i === 0 ? 'Blue' : 'Red'}`}
+            key={`${i}${_.id}`}
+            style={{
+              left: getLeft(_.flight.date),
+              opacity,
+            }}
+          />
+        ))
+      )}
       <div
         className="Plane"
+        ref={dragRef}
         style={{left: getLeft(currentFeature.flight.date)}}
       />
     </div>
@@ -38,4 +61,12 @@ export function Timeline({
     const left = 5 + (90 * (date.getTime() - minDate)) / (maxDate - minDate)
     return `${left}%`
   }
+}
+
+export default function Wrapper(props: Props): React.ReactElement {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Timeline {...props} />
+    </DndProvider>
+  )
 }
